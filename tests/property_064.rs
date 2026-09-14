aad98227254fc380ccc71a55a70574fffd8b273b
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-14 14:50:43 +0530

    code: add property_064.rs component

---
+++ tests/property_064.rs
@@ -0,0 +1,8 @@
+use forgequeue::priority;
+
+#[test]
+fn priority_064_range_contains_default() {
+    assert!(priority::MIN <= 0 && priority::MAX >= 0);
+    assert!(priority::MIN < priority::MAX);
+}
+
