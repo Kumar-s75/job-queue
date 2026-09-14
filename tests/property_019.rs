4b38907893df6640e8ba8898edd95ac132b8a2f4
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-09-14 14:50:38 +0530

    code: add property_019.rs component

---
+++ tests/property_019.rs
@@ -0,0 +1,8 @@
+use forgequeue::priority;
+
+#[test]
+fn priority_019_range_contains_default() {
+    assert!(priority::MIN <= 0 && priority::MAX >= 0);
+    assert!(priority::MIN < priority::MAX);
+}
+
